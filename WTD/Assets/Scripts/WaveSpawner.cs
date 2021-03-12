e1db7abda955b84901e97521d8707d271ca770dd
kodekoba <ordonez.adriel@gmail.com>
2021-03-12 03:17:00 -0800

    reupload 031221 0300

---
+++ WTD/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5.8f;
+    private float countdown = 3f;
+    private int waveIndex = 0;
+    public Text waveCountdownText;
+
+    void Update() {
+        if (countdown <= 0f) {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+    IEnumerator SpawnWave() {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
